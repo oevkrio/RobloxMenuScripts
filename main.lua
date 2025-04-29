e7712692047ac5a49ac6a3320e67df499fb75c17
Evkrio <officialsyzdfsdlsn@gmail.com>
2025-04-29 20:06:40 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/" ..game.PlaceId.. "/universe")).universeId
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 
 if game.PlaceId == 4483381587 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/oevkrio/RobloxMenuScripts/refs/heads/main/test.lua"))()
