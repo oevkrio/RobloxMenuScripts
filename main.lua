2d15b258017f27ca710a2e3edbf5bd2729c46e69
Evkrio <officialsyzdfsdlsn@gmail.com>
2025-04-29 19:29:19 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/" ..game.PlaceId.. "/universe")).universeId
+
+if game.PlaceId == 4483381587 then
+    loadstring(game:HttpGet("Your Github Raw Url"))()
+end
