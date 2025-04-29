1f3b63d593f4aeb9e9f383465aaf812f065be62d
Evkrio <officialsyzdfsdlsn@gmail.com>
2025-04-29 20:03:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/" ..game.PlaceId.. "/universe")).universeId
 
 if game.PlaceId == 4483381587 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/oevkrio/RobloxMenuScripts/refs/heads/main/test.lua"))()
+else:
+    LocalPlayer:Kick("This game is currently not supported.")
 end
