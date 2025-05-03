216eb1429d5c23941ab3b9ef3a70ded5d1fc00ec
Evkrio <officialsyzdfsdlsn@gmail.com>
2025-05-03 12:57:37 +0700

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,65 @@
+local httpService = game:GetService("HttpService")
+
+local InterfaceManager = {} do
+    InterfaceManager.Settings = {
+        Theme = "Dark",
+        Acrylic = true,
+        Transparency = true,
+        MenuKeybind = "LeftControl"
+    }
+
+    function InterfaceManager:SetLibrary(library)
+        self.Library = library
+    end
+
+    function InterfaceManager:BuildInterfaceSection(tab)
+        assert(self.Library, "Must set InterfaceManager.Library")
+        local Library = self.Library
+        local Settings = InterfaceManager.Settings
+
+        local section = tab:AddSection("Interface")
+
+        local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
+            Title = "Theme",
+            Description = "Changes the interface theme.",
+            Values = Library.Themes,
+            Default = Settings.Theme,
+            Callback = function(Value)
+                Library:SetTheme(Value)
+                Settings.Theme = Value
+            end
+        })
+
+        InterfaceTheme:SetValue(Settings.Theme)
+
+        if Library.UseAcrylic then
+            section:AddToggle("AcrylicToggle", {
+                Title = "Acrylic",
+                Description = "The blurred background requires graphic quality 8+",
+                Default = Settings.Acrylic,
+                Callback = function(Value)
+                    Library:ToggleAcrylic(Value)
+                    Settings.Acrylic = Value
+                end
+            })
+        end
+
+        section:AddToggle("TransparentToggle", {
+            Title = "Transparency",
+            Description = "Makes the interface transparent.",
+            Default = Settings.Transparency,
+            Callback = function(Value)
+                Library:ToggleTransparency(Value)
+                Settings.Transparency = Value
+            end
+        })
+
+        local MenuKeybind = section:AddKeybind("MenuKeybind", { Title = "Minimize Bind", Default = Settings.MenuKeybind })
+        MenuKeybind:OnChanged(function()
+            Settings.MenuKeybind = MenuKeybind.Value
+        end)
+        Library.MinimizeKeybind = MenuKeybind
+    end
+end
+
+return InterfaceManager
